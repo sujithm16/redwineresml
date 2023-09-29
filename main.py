6435fcfffdb16c8c9c79688c87a4d830cf895696
Sujith M <sujithm1610@gmail.com>
2023-09-29 18:55:49 +0530

    data ingestion

---
+++ main.py
@@ -0,0 +1,14 @@
+from redwine import logger
+from redwine.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
--- redwine/constants/__init__.py
+++ redwine/constants/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("D:\model deploy end to end\redwineresml\config\config.yaml")
-PARAMS_FILE_PATH = Path("D:\model deploy end to end\redwineresml\params.yaml")
-SCHEMA_FILE_PATH = Path("D:\model deploy end to end\redwineresml\schema.yaml")
+CONFIG_FILE_PATH = Path(r"D:\model deploy end to end\redwineresml\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"D:\model deploy end to end\redwineresml\params.yaml")
+SCHEMA_FILE_PATH = Path(r"D:\model deploy end to end\redwineresml\schema.yaml")

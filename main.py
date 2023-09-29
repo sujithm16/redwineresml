00e92ee6960709e10d3e1519ae43532d17dc5e31
Sujith M <sujithm1610@gmail.com>
2023-09-29 20:14:13 +0530

    model trainer

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from redwine import logger
 from redwine.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
 from redwine.pipeline.stage2_data_validation import DataValidationTrainingPipeline
 from redwine.pipeline.stage3_data_transformation import DataTransformationTrainingPipeline
+from redwine.pipeline.stage4_model_trainer import ModelTrainingPipeline
+
 
 
 
@@ -31,8 +33,20 @@ STAGE_NAME = "Data Transformation stage"
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataTransformationTrainingPipeline()
-        obj.main()
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Model Trainer stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_train = ModelTrainingPipeline()
+        model_train.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
         logger.exception(e)
---
+++ redwine/pipeline/stage4_model_trainer.py
@@ -0,0 +1,26 @@
+from redwine import logger
+from redwine.config.configuration import ConfigurationManager
+from redwine.components.model_trainer import ModelTrainer
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e

90cd0441400388a7a160fded7b30dba958a14aa6
Sujith M <sujithm1610@gmail.com>
2023-09-29 19:37:05 +0530

    data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from redwine import logger
 from redwine.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
 from redwine.pipeline.stage2_data_validation import DataValidationTrainingPipeline
+from redwine.pipeline.stage3_data_transformation import DataTransformationTrainingPipeline
 
 
 
@@ -24,3 +25,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ redwine/pipeline/stage3_data_transformation.py
@@ -0,0 +1,26 @@
+from redwine import logger
+from redwine.config.configuration import ConfigurationManager
+from redwine.components.data_transformation import DataTransformation
+
+
+STAGE_NAME = "Data Transformation stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.train_test_spliting()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e

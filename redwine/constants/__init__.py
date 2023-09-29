42e0daa2162fc80e83daa5cb63a111f968dc14f0
Sujith M <sujithm1610@gmail.com>
2023-09-29 18:28:23 +0530

    files created

---
+++ redwine/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("D:\model deploy end to end\redwineresml\config\config.yaml")
+PARAMS_FILE_PATH = Path("D:\model deploy end to end\redwineresml\params.yaml")
+SCHEMA_FILE_PATH = Path("D:\model deploy end to end\redwineresml\schema.yaml")
